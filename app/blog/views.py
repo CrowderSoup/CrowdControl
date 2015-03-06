7f315e37a62a9f019c8d56523f99e0205ff72ac9
Aaron Crowder <aaron@aaroncrowder.com>
2015-03-06 01:05:14 -0700

    Made some minor changes to the blog (added an atom feed)

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, abort
+from werkzeug.contrib.atom import AtomFeed
+from flask import render_template, abort, request
 from ..models import BlogPost, BlogCategory, Menu, User
 from . import blog
 import CommonMark
@@ -20,55 +21,55 @@ def the_blog(page):
 
     posts = []
 
-    for blogPost in blog_posts.items:
-        parsed = parser.parse(blogPost.content)
+    for post in blog_posts.items:
+        parsed = parser.parse(post.content)
         rendered = renderer.render(parsed)
 
         # Get username for by-line
-        by = User.query.filter_by(id=blogPost.user_id).first().username or "Unknown"
+        by = User.query.filter_by(id=post.user_id).first().username or "Unknown"
 
         posts.append({
-            'title': blogPost.title,
-            'slug': blogPost.slug,
-            'published_on': blogPost.published_on,
+            'title': post.title,
+            'slug': post.slug,
+            'published_on': post.published_on,
             'content': rendered,
             'by': by
         })
 
     # Let's return the page and menu items
-    return render_template("blog/blog.html", menu=main_menu, blogPosts=blog_posts, posts=posts, categories=categories)
+    return render_template("blog/blog.html", menu=main_menu, blog_posts=blog_posts, posts=posts, categories=categories)
 
 
 @blog.route('/blog/post/<path:slug>')
 def blog_post(slug):
     # Get the menu we want to use for this page...
     # TODO: make this more dynamic... probably tie it to the page
-    mainMenu = Menu.query.filter_by(name="Main").first()
+    main_menu = Menu.query.filter_by(name="Main").first()
 
-    blogPost = BlogPost.query.filter_by(slug=slug).first()
+    the_post = BlogPost.query.filter_by(slug=slug).first()
 
-    if blogPost is None:
+    if the_post is None:
         abort(404)
 
     # Markdown Parser and Renderer
     parser = CommonMark.DocParser()
     renderer = CommonMark.HTMLRenderer()
 
-    parsed = parser.parse(blogPost.content)
+    parsed = parser.parse(the_post.content)
     rendered = renderer.render(parsed)
 
     # Get username for by-line
-    by = User.query.filter_by(id=blogPost.user_id).first().username or "Unknown"
+    by = User.query.filter_by(id=the_post.user_id).first().username or "Unknown"
 
     post = {
-        'title': blogPost.title,
-        'slug': blogPost.slug,
-        'published_on': blogPost.published_on,
+        'title': the_post.title,
+        'slug': the_post.slug,
+        'published_on': the_post.published_on,
         'content': rendered,
         'by': by
     }
 
-    return render_template("blog/blog_post.html", menu=mainMenu, blogPost=post)
+    return render_template("blog/blog_post.html", menu=main_menu, blog_post=post, category=None)
 
 
 @blog.route('/blog/category/<path:slug>', defaults={'page': 1})
@@ -93,20 +94,49 @@ def blog_category(slug, page):
 
     posts = []
 
-    for blogPost in blog_posts.items:
-        parsed = parser.parse(blogPost.content)
+    for post in blog_posts.items:
+        parsed = parser.parse(post.content)
         rendered = renderer.render(parsed)
 
         # Get username for by-line
-        by = User.query.filter_by(id=blogPost.user_id).first().username or "Unknown"
+        by = User.query.filter_by(id=post.user_id).first().username or "Unknown"
 
         posts.append({
-            'title': blogPost.title,
-            'slug': blogPost.slug,
-            'published_on': blogPost.published_on,
+            'title': post.title,
+            'slug': post.slug,
+            'published_on': post.published_on,
             'content': rendered,
             'by': by
         })
 
     # Let's return the page and menu items
-    return render_template("blog/blog.html", menu=main_menu, blogPosts=blog_posts, posts=posts, categories=categories)
+    return render_template("blog/blog.html", menu=main_menu, blog_posts=blog_posts,
+                           posts=posts, categories=categories, category=category)
+
+
+@blog.route('/blog/feed.atom')
+def blog_feed():
+    feed = AtomFeed('CrowderSoup.com Blog',
+                    feed_url=request.url,
+                    url=request.url_root)
+
+    # Markdown Parser and Renderer
+    parser = CommonMark.DocParser()
+    renderer = CommonMark.HTMLRenderer()
+
+    blog_posts = BlogPost.query.order_by(BlogPost.published_on.desc()).paginate(1, 5)
+    for post in blog_posts.items:
+        parsed = parser.parse(post.content)
+        rendered = renderer.render(parsed)
+
+        # Get username for by-line
+        by = User.query.filter_by(id=post.user_id).first().username or "Unknown"
+
+        feed.add(post.title, rendered,
+                 content_type='html',
+                 author=by,
+                 url='{0}/blog/{1}'.format(request.url_root, post.slug),
+                 updated=post.published_on,
+                 published=post.published_on)
+
+    return feed.get_response()
