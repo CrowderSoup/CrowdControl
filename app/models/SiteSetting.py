e7f1cbe7df41f227598851cfcda9ecd5f8cf846f
Aaron Crowder <aaron@aaroncrowder.com>
2015-06-30 10:38:25 -0600

    Fixed migration, forgot to add primary key to site settings.

--- app/models/SiteSetting.py
+++ app/models/SiteSetting.py
@@ -4,5 +4,6 @@ from app import db
 class SiteSetting(db.Model):
     __tablename__ = 'sitesettings'
 
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True)
     value = db.Column(db.String())
