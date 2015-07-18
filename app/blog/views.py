b0b5e422747ed35a707170471c3194ce3cca9580
Aaron Crowder <aaron@aaroncrowder.com>
2015-07-17 22:52:57 -0600

    Added an RSS Feed using rFeed (https://github.com/svpino/rfeed) Fixed a couple of bugs with the blog related site settings.

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from werkzeug.contrib.atom import AtomFeed
-from flask import render_template, abort
+from rfeed import *
+from flask import render_template, abort, url_for, request, make_response
 from app.models.BlogPost import BlogPost
 from app.models.BlogCategory import BlogCategory
 from app.models.Menu import Menu
@@ -60,9 +61,18 @@ def the_blog(page):
 
 @blog.route('/blog/post/<path:slug>')
 def blog_post(slug):
+    # Get all the site settings
+    site_settings = SiteSetting.query.all()
+    settings = {}
+    for setting in site_settings:
+        settings[setting.name] = setting.value
+
     # Get the menu we want to use for this page...
-    # TODO: make this more dynamic... probably tie it to the page
-    main_menu = Menu.query.filter_by(name="Main").first()
+    if 'blog_menu' in settings and \
+        settings['blog_menu'] is not None:
+        main_menu = Menu.query.filter_by(id=int(settings['blog_menu'])).first()
+    else:
+        main_menu = Menu()
 
     the_post = BlogPost.query.filter_by(slug=slug).first()
 
@@ -87,7 +97,8 @@ def blog_post(slug):
         'by': by
     }
 
-    return render_template("blog/blog_post.html", menu=main_menu, blog_post=post, category=None)
+    return render_template("blog/blog_post.html", menu=main_menu, \
+                            blog_post=post, category=None, settings=settings)
 
 
 @blog.route('/blog/category/<path:slug>', defaults={'page': 1})
@@ -98,12 +109,22 @@ def blog_category(slug, page):
     if category is None:
         abort(404)
 
-    # Get the menu we want to use for this page...
-    # TODO: make this more dynamic... probably tie it to the page
-    main_menu = Menu.query.filter_by(name="Main").first()
+    # Get all the site settings
+    site_settings = SiteSetting.query.all()
+    settings = {}
+    for setting in site_settings:
+        settings[setting.name] = setting.value
 
-    blog_posts = BlogPost.query.filter(BlogPost.blogcategory_id == category.id, BlogPost.blogpoststatus_id == 2,
-                                       BlogPost.published_on <= datetime.utcnow())\
+    # Get the menu we want to use for this page...
+    if 'blog_menu' in settings and \
+        settings['blog_menu'] is not None:
+        main_menu = Menu.query.filter_by(id=int(settings['blog_menu'])).first()
+    else:
+        main_menu = Menu()
+
+    blog_posts = BlogPost.query.filter(BlogPost.blogcategory_id == category.id, \
+                                    BlogPost.blogpoststatus_id == 2,
+                                    BlogPost.published_on <= datetime.utcnow())\
         .order_by(BlogPost.published_on.desc()).paginate(page, 5)
     categories = BlogCategory.query.all()
 
@@ -129,13 +150,55 @@ def blog_category(slug, page):
         })
 
     # Let's return the page and menu items
-    return render_template("blog/blog.html", menu=main_menu, blog_posts=blog_posts,
-                           posts=posts, categories=categories, category=category)
+    return render_template("blog/blog.html", menu=main_menu, \
+                            blog_posts=blog_posts, posts=posts, \
+                            categories=categories, category=category, \
+                            settings=settings)
+
+
+@blog.route('/blog/feed.rss')
+def blog_rss():
+    root_url = '{0}blog'.format(request.url_root)
+    feed = Feed(title = "CrowderSoup.com Blog",
+                link = '{0}/feed.rss'.format(root_url),
+                description = "This is the blog description",
+                language = "en-US",
+                lastBuildDate = datetime.utcnow(),
+                items = [])
+
+    # Markdown Parser and Renderer
+    parser = CommonMark.DocParser()
+    renderer = CommonMark.HTMLRenderer()
+
+    blog_posts = BlogPost.query.order_by(BlogPost.published_on.desc())\
+                                                                .paginate(1, 5)
+
+    for post in blog_posts.items:
+        parsed = parser.parse(post.content)
+        rendered = renderer.render(parsed)
+
+        #Get username for by-line
+        by = User.query.filter_by(id=post.user_id).first().username or "Unknown"
+        post_url = "{0}/{1}".format(root_url, post.slug)
+
+        item = Item(title = post.title,
+                    link = post_url,
+                    description = '<![CDATA[{0}\r\n]]>'.format(rendered),
+                    author = by,
+                    guid = Guid(post_url),
+                    pubDate = post.published_on)
+
+        feed.items.append(item)
+
+    response= make_response(feed.rss())
+    response.headers["Content-Type"] = "application/xml"
+
+    return response
 
 
 @blog.route('/blog/feed.atom')
 def blog_feed():
-    root_url = 'http://crowdersoup.com/blog'
+    root_url = '{0}/blog'.format(request.url_root)
     feed = AtomFeed('CrowderSoup.com Blog',
                     feed_url='{0}/feed.atom'.format(root_url),
                     url=root_url)
@@ -144,7 +207,9 @@ def blog_feed():
     parser = CommonMark.DocParser()
     renderer = CommonMark.HTMLRenderer()
 
-    blog_posts = BlogPost.query.order_by(BlogPost.published_on.desc()).paginate(1, 5)
+    blog_posts = BlogPost.query.order_by(BlogPost.published_on.desc())\
+                                                                .paginate(1, 5)
+
     for post in blog_posts.items:
         parsed = parser.parse(post.content)
         rendered = renderer.render(parsed)
